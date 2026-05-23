3799402375ffe821f79a6300dbae397a6771ce89
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2026-05-23 21:10:05 +0300

    Add coupon routes and controller

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+
 
 import { connectDB } from "./lib/db.js";
 
@@ -22,6 +24,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 
 app.listen(PORT, () => {
