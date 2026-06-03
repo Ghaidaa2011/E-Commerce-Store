86bf2e83b4887d1fe023b662826369a2553f4c09
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2026-06-03 16:15:14 +0300

    update the server for production 3

--- backend/server.js
+++ backend/server.js
@@ -29,11 +29,12 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("/:path*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+app.use((req, res, next) => {
+  if (req.url.startsWith('/api')) {
+    return next();
+  }
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 }
 
 app.listen(PORT, () => {
